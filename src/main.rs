295a8abd7497c188c0995bf397b459b4f29df229
Walther <veeti.haapsamo@gmail.com>
2022-04-29 15:51:43 +0300

    Add simple sphere collision

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ use bevy::{core::FixedTimestep, prelude::*};
 /// Lockstep for the game engine
 const TIME_STEP: f32 = 1.0 / 60.0;
 
+/// Initial upwareds velocity for the jump
 const JUMP_INITIAL_VELOCITY: f32 = 5.0;
+/// Gravity constant for the jump
 const GRAVITY: f32 = 5.0;
+
+/// Default movement speed in the autoscroller
 const SCROLL_VELOCITY: f32 = 1.0;
+/// Boost velocity when the boost button is pressed
+const BOOST_VELOCITY: f32 = 2.0;
+
+/// Radius of the spheres, both for player and obstacles
+const SPHERE_RADIUS: f32 = 0.5;
 
 /// Fake unit for font-related calculations for visual consistency
 const REM: f32 = 24.0;
@@ -16,14 +25,17 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
+        .add_event::<CollisionEvent>()
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
                 .with_system(player_movement_system)
                 .with_system(camera_movement_system)
+                .with_system(check_for_collisions)
                 .with_system(fps_text_update_system)
                 .with_system(score_text_update_system),
         )
+        .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
 
@@ -34,32 +46,35 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    // spheres to jump over
+    // spheres to jump over. TODO: procedural generation
     let y = 0;
     for x in 0..=5 {
         let x01 = (x) as f32 / 10.0;
         let y01 = (y) as f32 / 4.0;
-        commands.spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 0.45,
-                subdivisions: 32,
-            })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::hex("777777").unwrap(),
-                // vary key PBR parameters on a grid of spheres to show the effect
-                metallic: y01,
-                perceptual_roughness: x01,
+        commands
+            .spawn_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Icosphere {
+                    radius: SPHERE_RADIUS,
+                    subdivisions: 32,
+                })),
+                material: materials.add(StandardMaterial {
+                    base_color: Color::hex("777777").unwrap(),
+                    // vary key PBR parameters on a grid of spheres to show the effect
+                    metallic: y01,
+                    perceptual_roughness: x01,
+                    ..Default::default()
+                }),
+                transform: Transform::from_xyz(x as f32 * 3.0, y as f32, 0.0),
                 ..Default::default()
-            }),
-            transform: Transform::from_xyz(x as f32 * 3.0, y as f32, 0.0),
-            ..Default::default()
-        });
+            })
+            .insert(Obstacle)
+            .insert(Collider);
     }
     // player
     commands
         .spawn_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 0.45,
+                radius: SPHERE_RADIUS,
                 subdivisions: 32,
             })),
             material: materials.add(StandardMaterial {
@@ -93,7 +108,7 @@ fn setup(
             },
             ..OrthographicCameraBundle::new_3d()
         })
-        .insert(Camera);
+        .insert(Camera::default());
 
     // UI camera
     commands.spawn_bundle(UiCameraBundle::default());
@@ -180,6 +195,8 @@ fn setup(
 #[derive(Component)]
 struct Player {
     jumping: JumpState,
+    collided: bool,
+    velocity_x: f32,
     velocity_y: f32,
 }
 
@@ -187,7 +204,9 @@ impl Default for Player {
     fn default() -> Self {
         Self {
             jumping: JumpState::OnFloor,
+            velocity_x: SCROLL_VELOCITY,
             velocity_y: 0.0,
+            collided: false,
         }
     }
 }
@@ -197,6 +216,9 @@ enum JumpState {
     InAir,
 }
 
+#[derive(Component)]
+struct Obstacle;
+
 // A unit struct to help identify the FPS UI component, since there may be many Text components
 #[derive(Component)]
 struct FpsText;
@@ -205,23 +227,31 @@ struct FpsText;
 #[derive(Component)]
 struct ScoreText;
 
+#[derive(Component)]
+struct Collider;
+
+#[derive(Default)]
+struct CollisionEvent;
+
 fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&mut Player, &mut Transform)>,
 ) {
     let (mut player, mut transform) = query.single_mut();
-    // x direction
-    let mut direction_x = SCROLL_VELOCITY;
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction_x -= 1.0;
+
+    if player.collided {
+        return;
     }
 
+    // x direction
     if keyboard_input.pressed(KeyCode::Right) {
-        direction_x += 1.0;
+        player.velocity_x = BOOST_VELOCITY;
+    } else {
+        player.velocity_x = SCROLL_VELOCITY;
     }
 
     let translation = &mut transform.translation;
-    translation.x += direction_x * TIME_STEP;
+    translation.x += player.velocity_x * TIME_STEP;
 
     // y direction
     if keyboard_input.pressed(KeyCode::Space) {
@@ -253,24 +283,37 @@ fn player_movement_system(
 
 // TODO: remove duplicate code...
 #[derive(Component)]
-struct Camera;
+struct Camera {
+    velocity_x: f32,
+    stopped: bool,
+}
+
+impl Camera {
+    fn default() -> Self {
+        Camera {
+            velocity_x: SCROLL_VELOCITY,
+            stopped: false,
+        }
+    }
+}
 
 fn camera_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Camera, &mut Transform)>,
+    mut query: Query<(&mut Camera, &mut Transform)>,
 ) {
-    let (_camera, mut transform) = query.single_mut();
-    let mut direction = SCROLL_VELOCITY;
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction -= 1.0;
+    let (mut camera, mut transform) = query.single_mut();
+    if camera.stopped {
+        return;
     }
 
     if keyboard_input.pressed(KeyCode::Right) {
-        direction += 1.0;
+        camera.velocity_x = BOOST_VELOCITY;
+    } else {
+        camera.velocity_x = SCROLL_VELOCITY;
     }
 
     let translation = &mut transform.translation;
-    translation.x += direction * TIME_STEP;
+    translation.x += camera.velocity_x * TIME_STEP;
 }
 
 fn fps_text_update_system(
@@ -293,4 +336,25 @@ fn score_text_update_system(
     let (_player, transform) = playertransform_query.single_mut();
     let mut scoretext = query.single_mut();
     scoretext.sections[1].value = format!("{:.2}", transform.translation.x);
+}
+
+fn check_for_collisions(
+    mut player_query: Query<(&mut Player, &Transform)>,
+    collider_query: Query<(Entity, &Transform, Option<&Obstacle>), With<Collider>>,
+    mut collision_events: EventWriter<CollisionEvent>,
+    mut camera_query: Query<&mut Camera>,
+) {
+    let (mut player, player_trans) = player_query.single_mut();
+    let mut camera = camera_query.single_mut();
+    let (x1, y1) = (player_trans.translation.x, player_trans.translation.y);
+    // Simple sphere collision based on center and radius
+    for (_sphere_ent, sphere_trans, _sphere_obs) in collider_query.iter() {
+        let (x2, y2) = (sphere_trans.translation.x, sphere_trans.translation.y);
+        let distance = ((x2 - x1).powf(2.0) + (y2 - y1).powf(2.0)).sqrt();
+        if distance <= SPHERE_RADIUS * 2.0 {
+            collision_events.send_default();
+            player.collided = true;
+            camera.stopped = true;
+        }
+    }
 }
